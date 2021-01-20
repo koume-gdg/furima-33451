8465864c5f12e065145ef3e6c5a9e8d51c329adb
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-20 21:52:19 +0900

    rubocop　-aを実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action:configure_parmitted_parameters, if: :devise_controller?
-
+  before_action :configure_parmitted_parameters, if: :devise_controller?
 
   private
+
   def configure_parmitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,
+                                             :birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-         
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i #[a-z\d]は英数字,{6,}は下限と上限、/iは大文字も小文字も含む
-         VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/ #全角カタカナ
-         VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/ #全角ひらがな、カタカナ、漢字
 
-         validates :nickname,           presence: true
-         validates :family_name,        presence: true,  format: {with: VALID_NAME_REGEX}
-         validates :first_name,         presence: true,  format: {with: VALID_NAME_REGEX}
-         validates :family_name_kana,   presence: true, format: {with: VALID_KANA_REGEX}
-         validates :first_name_kana,    presence: true, format: {with: VALID_KANA_REGEX}
-         validates :birthday,           presence: true
-         validates :password,           format: {with: VALID_PASSWORD_REGEX}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i # [a-z\d]は英数字,{6,}は下限と上限、/iは大文字も小文字も含む
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/ # 全角カタカナ
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/ # 全角ひらがな、カタカナ、漢字
 
+  validates :nickname,           presence: true
+  validates :family_name,        presence: true,  format: { with: VALID_NAME_REGEX }
+  validates :first_name,         presence: true,  format: { with: VALID_NAME_REGEX }
+  validates :family_name_kana,   presence: true, format: { with: VALID_KANA_REGEX }
+  validates :first_name_kana,    presence: true, format: { with: VALID_KANA_REGEX }
+  validates :birthday,           presence: true
+  validates :password,           format: { with: VALID_PASSWORD_REGEX }
 end
