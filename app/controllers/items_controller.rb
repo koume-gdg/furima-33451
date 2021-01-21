1d2101cae1c4322f9fee975985992301d5375de4
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-21 16:15:45 +0900

    active hash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,10 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+  
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :price, :description,:status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :category_id).merge(user_id: current_user.id)
