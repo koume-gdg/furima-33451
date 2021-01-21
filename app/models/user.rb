ae1a7d72b1c9e2261c83bd6f3de5aa7ddba7b89a
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-21 10:31:39 +0900

    バリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,14 @@ class User < ApplicationRecord
   VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/ # 全角カタカナ
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/ # 全角ひらがな、カタカナ、漢字
 
-  validates :nickname,           presence: true
-  validates :family_name,        presence: true,  format: { with: VALID_NAME_REGEX }
-  validates :first_name,         presence: true,  format: { with: VALID_NAME_REGEX }
-  validates :family_name_kana,   presence: true, format: { with: VALID_KANA_REGEX }
-  validates :first_name_kana,    presence: true, format: { with: VALID_KANA_REGEX }
-  validates :birthday,           presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name,      format: { with: VALID_NAME_REGEX }
+    validates :first_name,       format: { with: VALID_NAME_REGEX }
+    validates :family_name_kana, format: { with: VALID_KANA_REGEX }
+    validates :first_name_kana,  format: { with: VALID_KANA_REGEX }
+    validates :birthday
+  end
+
   validates :password,           format: { with: VALID_PASSWORD_REGEX }
 end
