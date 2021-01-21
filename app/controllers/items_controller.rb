5813cc1b010791043d002117109af4a6faaefc03
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-21 11:54:43 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :description,:status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :category_id).merge(user_id: current_user.id)
 end
