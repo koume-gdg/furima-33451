9722cd8dfd7a7d607d2f8c047305a53460891bf6
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-21 17:09:38 +0900

    都道府県のプルダウン導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,11 @@ class ItemsController < ApplicationController
 
   def new
   end
-  
+
 
   private
   def item_params
     params.require(:item).permit(:name, :image, :price, :description,:status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :category_id).merge(user_id: current_user.id)
+  end
+
 end
