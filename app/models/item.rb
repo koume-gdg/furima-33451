3883c9a02d2dae0ddd514ee89ca9e818ef7f0d22
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-21 18:20:35 +0900

    プルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :prefecture
+  belongs_to :shipping_cost
+  belongs_to :shipping_day
+  belongs_to :status
+  belongs_to :category
 
 end
