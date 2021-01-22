c4819f5675963c679c656d1cbb924ee3bd909800
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-22 14:15:47 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,18 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :category
 
+  VALID_PRICE_REGEX = /\A[0-9]+\z/ #半角数字
+
+  with_options presence: true do
+    validates :image
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price,            format: { with: VALID_PRICE_REGEX }, inclusion: {in: 300..9999999}
+  end
+  
+
 end
