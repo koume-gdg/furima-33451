fc28272223e5ebb71ddcb25e10e18048bdc2849b
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-23 11:49:12 +0900

    投稿を保存する

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Name.name }
+    description{ 'サンプル説明です' }
+    category { Category.all.sample }
+    status { Status.all.sample }
+    shipping_cost { Shipping_cost.all.sample }
+    prefecture { Prefecture.all.sample }
+    shipping_day { Shipping_day.all.sample }
+    price { '1000' }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品新規投稿する' do
+    context '投稿できるとき' do
+      it '全ての入力ができていれば投稿される' do
+        expect(@item).to be_valid
+      end
+    end
+    end
+    
+  end
+
+
+
+
 end
