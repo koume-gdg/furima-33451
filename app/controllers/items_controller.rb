7bba79472c8e1fab9144479ba42245d1b33dc8d7
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-25 11:06:26 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :contributor_confirmation, only:[:edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -37,9 +37,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     redirect_to root_path
-
   end
-
 
   private
 
@@ -47,6 +45,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :image, :price, :description, :status_id, :shipping_cost_id, :prefecture_id,
                                  :shipping_day_id, :category_id).merge(user_id: current_user.id)
   end
+
   def set_item
     @item = Item.find(params[:id])
   end
