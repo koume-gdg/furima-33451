c6b8d0d2ca7dba9defa4f961fbda3704d2805e03
koume-gdg <gdgpll.vmj@gmail.com>
2021-01-25 11:39:08 +0900

    条件式の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,9 +35,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
-  end
+    if @item.destroy
+      edirect_to root_path
+    else
+      render :show
+    end
 
   private
 
