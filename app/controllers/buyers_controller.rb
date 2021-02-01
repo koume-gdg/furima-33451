041a44f9bd6f64cf982b9e675688fe65853c11e1
koume-gdg <gdgpll.vmj@gmail.com>
2021-02-01 11:26:24 +0900

    フォームオブジェクト実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,20 +8,20 @@ class BuyersController < ApplicationController
     #インデックスのフォームのあとの変数　＝　フォームオブジェクトの名前.new
   end
 
-  def creat
+  def create
     @buyer_address = BuyerAddress.new(address_params)
     if @buyer_address.valid?
       @buyer_address.save
       redirect_to root_path
     else
-      render root_path
+      render :index
     end
   end
 
 
     private
     def address_params
-      params.require(:buyer_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number)
+      params.require(:buyer_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
     end
     
 
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,7 +1,7 @@
 class BuyerAddress
 
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :address, :city, :building, :phone_number
+  attr_accessor :post_code, :prefecture_id, :address, :city, :building, :phone_number, :item_id, :user_id
   
   VALID_POSTCODE_REGEX= /\A\d{3}[-]\d{4}\z/ #000-1234
   VALID_PHONENUMBER_REGEX= /\A\d{10,11}\z/ #ハイフンなし10桁or11桁電話番号
@@ -16,11 +16,11 @@ class BuyerAddress
   end
 
   def save
-    buyer = Buyer.create(user_id: user.id, item_id: item.id)
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
 
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, address: address, city: city, phone_number: phone_number)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, address: address, city: city, phone_number: phone_number, building: building, buyer_id: buyer.id)
 
-    Item.create(name: name, price: price)
+    #Item.create(name: name, price: price)
 
   end
 end
