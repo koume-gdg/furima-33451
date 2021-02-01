56d0009d02338085bca1d51cfcb71319964ac291
koume-gdg <gdgpll.vmj@gmail.com>
2021-02-01 12:41:49 +0900

    フォーム送信時に発火する

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("../calcurate");
+require("../calcurate")
+require("../card")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
