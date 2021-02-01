bc2d3e8defa23ef37bf6cb0e597b70a0cd2971a5
koume-gdg <gdgpll.vmj@gmail.com>
2021-02-01 21:18:33 +0900

    password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == 'admin' && password == '33451'
     end
   end
 end
